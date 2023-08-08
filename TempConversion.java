c011a3b209242438f9d53bf7fecd68a3af84550c
Mansi Rank <109585320+RankMansi@users.noreply.github.com>
2023-08-08 19:14:24 +0530

    Add files via upload

---
+++ TempConversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class TempConversion{
+
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		float C = scan.nextFloat();
+		float f=((9/5f)*C)+32;
+		System.out.println(f);
+	}
+}
